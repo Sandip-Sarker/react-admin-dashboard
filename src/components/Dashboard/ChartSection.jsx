d763a993b6ee5b0be0a6a7b275e1e3f8f0be6f8e
Sandip Sarker <sandip@softvence.com>
2026-06-16 14:50:25 +0600

    pie chart

--- src/components/Dashboard/ChartSection.jsx
+++ src/components/Dashboard/ChartSection.jsx
@@ -3,7 +3,7 @@ import RevnueChart from "./RevnueChart";
 
 const ChartSection = () => {
     return (
-        <div className="gird grid-cols-1 xl:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
             <div className="xl:col-span-2 col-span-2">
                 <RevnueChart/>
             </div>
