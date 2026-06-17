3258487c275c37beab57d84e0a8ad3d7c525f3eb
Sandip Sarker <sandip@softvence.com>
2026-06-17 11:56:14 +0600

    pie chart

--- src/components/Dashboard/ChartSection.jsx
+++ src/components/Dashboard/ChartSection.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import RevnueChart from "./RevnueChart";
+import SaleChart from "./SaleChart";
 
 const ChartSection = () => {
     return (
         <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
             <div className="xl:col-span-2 col-span-2">
                 <RevnueChart/>
+            </div>
+            <div>
+                <SaleChart/>
             </div>
         </div>
     )
--- src/components/Dashboard/SaleChart.jsx
+++ src/components/Dashboard/SaleChart.jsx
@@ -19,8 +19,13 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const SaleChart = () => {
   return (
-    <div className="sale-chart-card">
-      <h3>Sales Distribution</h3>
+    <div className="h-full bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl rounded-blur-2xl border border-slate-200/50 dark:border-slate-700/50 p-4">
+      <div>
+        <h3 className="text-xl font-bold text-slate-800 dark:text-white">Sales Distribution</h3>
+        <p className="text-sm text-slate-500 dark:text-slate-400">
+            Monthly sales and expenses
+        </p>
+      </div>
       <ResponsiveContainer width="100%" height={300}>
         <PieChart>
           <Pie
